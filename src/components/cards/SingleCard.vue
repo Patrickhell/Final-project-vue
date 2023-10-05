6b293a734e94df90647f82ac526b0ac9cd68514d
Patrick Hell <78173981+Patrickhell@users.noreply.github.com>
2023-10-05 16:23:31 +0200

    Initialize filter-progress

---
+++ src/components/cards/SingleCard.vue
@@ -0,0 +1,81 @@
+<template>
+        <a href="#" class="flex items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div class="image-container flex">
+                <img class="object-contain" :src="CardElement.thumb" alt="">
+            </div>
+                <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            </div>
+        </a>
+</template>
+<script>
+export default {
+    name: 'SingleCard',
+    props:{
+        CardElement:Object,
+    },
+}
+</script>
+<style lang="scss" scoped>
+div.image-container{
+    width: 40%;
+        img{
+            object-fit: cover;
+            width: 150px;
+            height: 150px;
+            border-radius: 50%;
+        }
+}
+
+@media screen and (min-width: 768px) {
+        div.image-container{
+        width: 60%;
+            img{
+                object-fit: cover;
+                width: 150px;
+                height: 150px;
+                border-radius: 50%;
+            }
+    }
+}
+
+@media screen and (min-width: 1024px) {
+        div.image-container{
+        width: 30%;
+            img{
+                object-fit: cover;
+                width: 150px;
+                height: 150px;
+                border-radius: 50%;
+                padding: .2rem;
+            }
+    }
+}
+
+@media screen and (min-width: 1280px) {
+        div.image-container{
+        width: 25%;
+            img{
+                object-fit: cover;
+                width: 180px;
+                height: 180px;
+                border-radius: 50%;
+                padding: .3rem;
+            }
+    }
+}
+
+@media screen and (min-width: 1536px) {
+        div.image-container{
+        width: 17%;
+            img{
+                object-fit: cover;
+                width: 180px;
+                height: 180px;
+                border-radius: 50%;
+                padding: .3rem;
+            }
+    }
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,82 @@
+<template>
+    
+    <div class="pt-20 columns-container mx-auto">
+        <div class="left-column my-7">
+            <div class="flex justify-center items-center flex-wrap">
+                <button type="button" v-for="n in 15" class="text-white focus:ring-4 focus:outline-none font-medium rounded-full text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 bd_btn">
+                    exemple button
+                </button>
+            </div>
+
+            <div>
+                
+            </div>
+
+        </div>
+
+        <div class="card-container">
+            <SingleCard v-for="itemDoctor in itemsDoctor" :CardElement="itemDoctor" class="mx-2 my-7 card-width"/>
+        </div>
+    </div>
+
+</template>
+<script>
+import SingleCard from "../components/cards/SingleCard.vue";
+
+export default {
+    name:"indexPage",
+
+    components:{
+        SingleCard,
+    },
+
+    data() {
+        return {
+            itemsDoctor:[
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel",
+                },]
+        }
+    },
+}
+</script>
+<style lang="scss" scoped>
+span{
+        padding: .15rem;
+    }
+    button{
+        margin: .2rem;
+    }
+     @media screen and (min-width: 1024px) {
+    div.columns-container{
+        display: flex;
+            div.left-column{
+                width: 800px;
+            }
+    }
+    div.card-container{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+    }
+    .card-width{
+        width: 90%;
+    }
+}
+</style>
